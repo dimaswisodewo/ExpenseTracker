9eb30839094de3225aaefe25526dc3cf7bb0898d
Dimas Wisodewo <dimaswisodewo@Dimas-MacBook-Pro.local>
2022-04-02 16:04:47 +0700

    [UPDATE] : Improve readibility on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'dart:html';
-// import 'dart:io';
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -130,6 +128,76 @@ class _MyHomePageState extends State<MyHomePage> {
     return formatter.format(income);
   }
 
+  List<Widget> _transactionContent(
+      MediaQueryData mediaQueryData, AppBar appBar) {
+    return [
+      _userTransactions.length > 0
+          ? Container(
+              height: 60,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Show Chart',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  Platform.isIOS
+                      ? CupertinoSwitch(
+                          value: _showChart,
+                          onChanged: (newValue) {
+                            if (newValue)
+                              _chartHeight = 250;
+                            else
+                              _chartHeight = 0;
+
+                            setState(() {
+                              _showChart = newValue;
+                            });
+                          },
+                        )
+                      : Switch(
+                          value: _showChart,
+                          onChanged: (newValue) {
+                            if (newValue)
+                              _chartHeight = 250;
+                            else
+                              _chartHeight = 0;
+
+                            setState(() {
+                              _showChart = newValue;
+                            });
+                          },
+                        )
+                ],
+              ),
+            )
+          : Container(),
+      _showChart == true
+          ? Container(
+              height: 250,
+              margin: const EdgeInsets.symmetric(horizontal: 3),
+              child: Chart(_recentTransactions),
+            )
+          : Container(
+              height: 0,
+            ),
+      Container(
+        height: (mediaQueryData.size.height -
+            mediaQueryData.padding.top -
+            appBar.preferredSize.height -
+            60 -
+            _chartHeight -
+            60 -
+            34),
+        child: TransactionList(
+          transactions: _userTransactions,
+          deleteTransaction: deleteTransaction,
+        ),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     // final ObstructingPreferredSizeWidget appBarIOS = CupertinoNavigationBar(
@@ -143,6 +211,8 @@ class _MyHomePageState extends State<MyHomePage> {
     //     ],
     //   ),
     // );
+
+    final MediaQueryData mediaQueryData = MediaQuery.of(context);
 
     final appBar = AppBar(
       title: const Text('Expense Tracker'),
@@ -165,110 +235,31 @@ class _MyHomePageState extends State<MyHomePage> {
             right: 22,
             left: 22,
           ),
-          // margin: EdgeInsets.only(bottom: 10),
           child: Row(
             children: [
               Expanded(
                 flex: 1,
                 child: Center(
-                  child: Text(
-                    getOutcome(),
-                    style: const TextStyle(
-                      color: Colors.red,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  child: Text(getOutcome(),
+                      style: const TextStyle(color: Colors.red)),
                 ),
               ),
               Expanded(
                 flex: 1,
                 child: Center(
-                  child: Text(
-                    getIncome(),
-                    style: const TextStyle(
-                      color: Colors.green,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  child: Text(getIncome(),
+                      style: const TextStyle(color: Colors.green)),
                 ),
               ),
             ],
           ),
         ),
-        _userTransactions.length > 0
-            ? Container(
-                height: 60,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Show Chart',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                        )),
-                    Platform.isIOS
-                        ? CupertinoSwitch(
-                            value: _showChart,
-                            onChanged: (newValue) {
-                              if (newValue)
-                                _chartHeight = 250;
-                              else
-                                _chartHeight = 0;
-
-                              setState(() {
-                                _showChart = newValue;
-                              });
-                            },
-                          )
-                        : Switch(
-                            value: _showChart,
-                            onChanged: (newValue) {
-                              if (newValue)
-                                _chartHeight = 250;
-                              else
-                                _chartHeight = 0;
-
-                              setState(() {
-                                _showChart = newValue;
-                              });
-                            })
-                  ],
-                ),
-              )
-            : Container(),
-        _showChart == true
-            ? Container(
-                height: 250,
-                margin: const EdgeInsets.symmetric(horizontal: 3),
-                child: Chart(_recentTransactions),
-              )
-            : Container(
-                height: 0,
-              ),
-        Container(
-          height: (MediaQuery.of(context).size.height -
-              MediaQuery.of(context).padding.top -
-              appBar.preferredSize.height -
-              60 -
-              _chartHeight -
-              60 -
-              34),
-          child: TransactionList(
-            transactions: _userTransactions,
-            deleteTransaction: deleteTransaction,
-          ),
-        ),
+        ..._transactionContent(
+            mediaQueryData, appBar), // Spread the element inside list
       ],
     );
 
-    return
-        //  Platform.isIOS
-        //     ? CupertinoPageScaffold(
-        //         navigationBar: appBarIOS,
-        //         child: SafeArea(child: appBody),
-        //       )
-        //     :
-        Scaffold(
+    return Scaffold(
       appBar: appBar,
       body: SafeArea(child: appBody),
       floatingActionButton: FloatingActionButton(
